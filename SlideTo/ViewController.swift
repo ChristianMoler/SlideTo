98a2339f87a181bd243fffcd194d4a41f365d771
Christian <christianmoler94@gmail.com>
2017-09-15 16:31:58 +0300

    init commit

---
+++ SlideTo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SlideTo
+//
+//  Created by Christian    Moler on 15.09.17.
+//  Copyright © 2017 Christian Moler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
