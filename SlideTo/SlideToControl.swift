d6be8cfc6bd6cfa4440adf4525f33865ea6c499e
Christian <christianmoler94@gmail.com>
2017-09-15 18:44:44 +0300

    Updated inspectable

--- SlideTo/SlideToControl.swift
+++ SlideTo/SlideToControl.swift
@@ -11,6 +11,37 @@ import UIKit
 @IBDesignable
 class SlideToControl: UIControl {
 
-
-
+  //MARK: Outlets
+  @IBOutlet private weak var thumbView: UIView!
+  @IBOutlet private weak var textLabel: UILabel!
+  var view: UIView?
+  
+  //Inspectable
+  
+  @IBInspectable var thumbViewColor: UIColor?{
+    set{
+      thumbView.backgroundColor = thumbViewColor
+    }
+    get{
+      return thumbView.backgroundColor
+    }
+  }
+  
+  //MARK: Internal methods
+  
+  private func xibSetup() {
+    
+  }
+  
+  func loadViewFromNib() -> UIView {
+    let bundle = Bundle(for: type(of: self))
+    let nib = UINib(nibName: "SlideToControl", bundle: bundle)
+    let view = nib.instantiate(withOwner: self, options: nil)[0] as! UIView
+    return view
+  }
 }
+
+
+
+
+
--- SlideTo/ViewController.swift
+++ SlideTo/ViewController.swift
@@ -10,10 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+  override func viewDidLoad() {
+      super.viewDidLoad()
+  }
+  
 
 }
 
