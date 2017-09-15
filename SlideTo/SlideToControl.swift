f935af7c5bec0f2e8dc29526665c2d8e5809697f
Christian <christianmoler94@gmail.com>
2017-09-15 16:29:36 +0300

    Init commit

---
+++ SlideTo/SlideToControl.swift
@@ -0,0 +1,16 @@
+//
+//  SlideToControl.swift
+//  SlideTo
+//
+//  Created by Christian    Moler on 15.09.17.
+//  Copyright © 2017 Christian Moler. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class SlideToControl: UIControl {
+
+
+
+}
