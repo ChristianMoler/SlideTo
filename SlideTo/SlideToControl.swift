0f5bc9a88b902f9325d5364e8f82db919f95d738
Christian <christianmoler94@gmail.com>
2017-09-18 16:08:43 +0300

    IB updated

--- SlideTo/SlideToControl.swift
+++ SlideTo/SlideToControl.swift
@@ -18,27 +18,93 @@ class SlideToControl: UIControl {
   
   //Inspectable
   
-  @IBInspectable var thumbViewColor: UIColor?{
-    set{
-      thumbView.backgroundColor = thumbViewColor
+  @IBInspectable var thumbColor: UIColor? {
+    set(color){
+      thumbView.backgroundColor = color
     }
     get{
       return thumbView.backgroundColor
     }
   }
+  @IBInspectable var bgColor: UIColor? {
+    set(color){
+      view?.backgroundColor = color
+    }
+    get{
+      return view?.backgroundColor
+    }
+  }
+  @IBInspectable var labelColor: UIColor? {
+    set(color){
+      textLabel.textColor = color
+    }
+    get{
+      return textLabel.textColor
+    }
+  }
+  @IBInspectable var labelText: String? {
+    set(text){
+      textLabel.text = text
+    }
+    get{
+      return textLabel.text
+    }
+  }
+  @IBInspectable var labelFontSize: CGFloat {
+    set(size){
+      textLabel.font = UIFont.systemFont(ofSize: size)
+    }
+    get{
+      return textLabel.font.pointSize
+    }
+  }
+
+  
+  
+  //MARK: Initializers
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    xibSetup()
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+    xibSetup()
+  }
+  
   
   //MARK: Internal methods
   
   private func xibSetup() {
-    
+    view = loadViewFromNib()
+    view?.frame = bounds
+    view?.autoresizingMask = [.flexibleWidth,.flexibleHeight]
+    layoutSubviews()
+    guard let resultView = view else {  return  }
+    addSubview(resultView)
   }
   
-  func loadViewFromNib() -> UIView {
+  private func loadViewFromNib() -> UIView {
     let bundle = Bundle(for: type(of: self))
     let nib = UINib(nibName: "SlideToControl", bundle: bundle)
     let view = nib.instantiate(withOwner: self, options: nil)[0] as! UIView
     return view
   }
+  
+  private func roundOutViewWith(height: CGFloat){
+
+  }
+  
+  override func layoutSubviews() {
+    guard let height = view?.frame.height else { return }
+    thumbView.layer.cornerRadius = (height * 0.8)/2
+    thumbView.layer.masksToBounds = true
+    view?.layer.cornerRadius = height/2
+    view?.layer.masksToBounds = true
+  }
+    
+
 }
 
 
